2d5b63bb0681db814c292815c987a336612bb7d4
Brian Gorrie <brgorrie@gmail.com>
2011-11-05 20:10:43 +0800

    Creating basic test cases

---
+++ clients/java/src/ConnectToServerTest.java
@@ -0,0 +1,11 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Brian Gorrie
+ * Date: 5/11/11
+ * Time: 8:09 PM
+ *
+ * Example of connecting to server.
+ *
+ */
+public class ConnectToServerTest {
+}
