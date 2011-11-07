b5d9ee2754edb624ab93e39c3da4f20e7b1b979d
Clint Webb <webb.clint@gmail.com>
2011-11-07 23:31:25 +0800

    Created gamut test suite.  pretty basic so far though

---
+++ gamut/c/gamut.c
@@ -0,0 +1,47 @@
+/*
+	Tool that tests the functionality of the libopencluster library.
+*/
+
+
+#include <assert.h>
+#include <opencluster.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+	cluster_t *cluster;
+	int data;
+	int result;
+
+	printf("Initialising the cluster library\n");
+	cluster = cluster_init(NULL);
+	
+	printf("Adding 127.0.0.1:13600 to the server list.\n");
+	cluster_addserver(cluster, "127.0.0.1:13600");
+	
+	if (cluster_connect(cluster)) {
+	
+		// set an item in the cluster with some data.
+		printf("Setting data in the cluster\n");
+		cluster_setint(cluster, "testdata", 45);
+		
+		// pull some data out of the cluster.
+		data = 0;
+		printf("Getting data from the cluster.\n");
+		result = cluster_getint(cluster, "testdata", &data);
+		
+		printf ("Result of 'testdata' in cache.  date=%d\n", data);
+		
+		printf("Disconnecting from the cluster,\n);
+		cluster_disconnect(cluster);		
+	}
+	
+	printf("Shutting down\n");
+	cluster = cluster_free(cluster);
+	assert(cluster == NULL);
+	
+	
+	return(0);
+}
+
