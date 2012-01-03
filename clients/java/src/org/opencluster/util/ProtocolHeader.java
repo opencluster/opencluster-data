e77441f13e59ad4ed3108bfa589795753888c009
Brian Gorrie <brgorrie@gmail.com>
2012-01-03 14:25:52 +0800

    Start of building test cases around the protocol piece

---
+++ clients/java/src/org/opencluster/util/ProtocolHeader.java
@@ -0,0 +1,52 @@
+package org.opencluster.util;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Brian Gorrie
+ * Date: 3/01/12
+ * Time: 1:16 PM
+ * <p/>
+ * All communications begin with an 12-byte header.
+ * ---------------------------------------------------------------------------------
+ * |   Short Int   | 2 bytes |  Command (or Reply) Identifier                      |
+ * |   Short Int   | 2 bytes |  Command being replied to (0 if not a reply)        |
+ * |   Integer     | 4 bytes |  User specified ID                                  |
+ * |   Integer     | 4 bytes |  Length of the data following this header           |
+ * ---------------------------------------------------------------------------------
+ */
+public class ProtocolHeader {
+
+    ProtocolCommand command = ProtocolCommand.NO_COMMAND;
+
+    ProtocolCommand replyToCommand = ProtocolCommand.NO_COMMAND;
+
+    int userSpecifiedID = 0;
+
+    int dataLength = 0;
+
+    public ProtocolHeader(ProtocolCommand command) {
+        this.command = command;
+    }
+
+    public ProtocolHeader(ProtocolCommand command, ProtocolCommand replyToCommand) {
+        this.command = command;
+        this.replyToCommand = replyToCommand;
+    }
+
+    public ProtocolHeader(ProtocolCommand command, ProtocolCommand replyToCommand, int userSpecifiedID, int dataLength) {
+        this.command = command;
+        this.replyToCommand = replyToCommand;
+        this.userSpecifiedID = userSpecifiedID;
+        this.dataLength = dataLength;
+    }
+
+    public void writeToByteBuffer(ByteBuffer buf) {
+        buf.putShort(command.getCode());
+        buf.putShort(replyToCommand.getCode());
+        buf.putInt(userSpecifiedID);
+        buf.putInt(dataLength);
+    }
+}
+
---
+++ clients/java/test/org/opencluster/util/ClusteredBooleanTest.java
@@ -0,0 +1,22 @@
+package org.opencluster.util;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Brian Gorrie
+ * Date: 15/12/11
+ * Time: 5:30 PM
+ *
+ * Test scenario for clustered boolean.
+ *
+ */
+public class ClusteredBooleanTest {
+
+    @Test
+    public void TestClassCreation() {
+        Assert.assertTrue("Class exists", new ClusteredBoolean("some-id",false) != null);
+    }
+
+}
