a29f4d9221d18a19372cc69e8a7797d90eb2bbb0
Brian Gorrie <brgorrie@gmail.com>
2012-03-31 17:39:14 +0800

    logging changes

--- server/client.c
+++ server/client.c
@@ -154,15 +154,6 @@ void client_free(client_t *client)
 		client->out.max = 0;
 	}
 
-        if (client->in.length >= 0 && _verbose > 2) {
-                pp =  client->in.buffer + client->in.offset;
-                printf("Data received.  length=%d\n", res);
-                for (processed=0; processed < res; processed++, pp++) {
-                        printf("  %04d: %02X (%d)\n", processed, *pp, *pp);
-                }
-        }
-
-
 	assert(client->in.length == 0);
 	assert(client->in.offset == 0);
 	if (client->in.buffer) {
