edab176a15c43bed4c67605baf66ae1eb4778645
Brian Gorrie <brgorrie@gmail.com>
2012-04-06 13:22:43 +0800

    Client now created up to the opening of the socket connection

--- clients/java/src/org/opencluster/client/Client.java
+++ clients/java/src/org/opencluster/client/Client.java
@@ -1,9 +1,72 @@
 package org.opencluster.client;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketOption;
+import java.net.StandardSocketOptions;
+import java.nio.channels.SocketChannel;
+import java.util.Set;
+import java.util.logging.Logger;
+
 /**
  * User: Brian Gorrie
  * Date: 1/04/12
  * Time: 9:33 PM
+ * <p/>
+ * This class is a client fo the cluster.
  */
 public class Client {
+
+    private final static Logger LOG = Logger.getLogger(Client.class.getName());
+
+    private String hostNameOrIP;
+    private int port;
+    private SocketChannel connection;
+    private int sendBufferSize;
+    private int recieveBufferSize;
+
+    public void Client() {
+        // Empty constructor
+    }
+
+    public void start() throws IOException {
+        // start the client.
+        connection = createConnection();
+    }
+
+    public void addServer(String hostNameOrIP, int port) {
+        this.hostNameOrIP = hostNameOrIP;
+        this.port = port;
+    }
+
+    private SocketChannel createConnection() throws IOException {
+        SocketChannel sChannel = null;
+        sChannel = SocketChannel.open();
+        sChannel.configureBlocking(false);
+
+        sendBufferSize = sChannel.getOption(StandardSocketOptions.SO_SNDBUF);
+        LOG.info("Using send buffer size: " + sendBufferSize);
+        recieveBufferSize = sChannel.getOption(StandardSocketOptions.SO_RCVBUF);
+        LOG.info("Using recieve buffer size: " + recieveBufferSize);
+
+        // Send a connection request to the server; this method is non-blocking
+        sChannel.connect(new InetSocketAddress(hostNameOrIP, port));
+
+        while (!sChannel.finishConnect()) {
+            LOG.info("Not connected yet, waiting for connection to complete.");
+            try {
+                Thread.sleep(100L);
+            } catch (InterruptedException e) {
+                //
+            }
+        }
+
+        Set<SocketOption<?>> socketOptions = sChannel.supportedOptions();
+        for(SocketOption<?> socketOption : socketOptions ) {
+            LOG.info("Supported option: " + socketOption.toString() +", Current Value: " + sChannel.getOption(socketOption));
+        }
+
+        return sChannel;
+    }
+
 }
--- clients/java/test/org/opencluster/client/ClientTest.java
+++ clients/java/test/org/opencluster/client/ClientTest.java
@@ -1,7 +1,14 @@
 package org.opencluster.client;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 /**
  * User: Brian Gorrie
@@ -10,13 +17,45 @@ import org.junit.Test;
  */
 public class ClientTest {
 
-    private static final String HOSTNAME = "localhost";
-    private static final int PORT = 7777;
+    private final static Logger LOG = Logger.getLogger(ClientTest.class.getName());
+
+    public static final String HOST_NAME_OR_IP = "localhost";
+    public static final int PORT = 7777;
 
     @Test
     public void TestClassExists() {
         Assert.assertTrue("Client Exists.", new Client() != null);
 
+    }
+
+    @Test
+    public void TestClient() {
+        setupLogLevel();
+        Client client = new Client();
+        client.addServer(HOST_NAME_OR_IP, PORT);
+        try {
+            client.start();
+        } catch (IOException e) {
+            logException(e, "Unable to start the client.");
+        }
+    }
+
+    private void logException(Throwable e, String msg) {
+        LOG.severe(msg);
+        LOG.severe(ExceptionUtils.getMessage(e));
+        LOG.severe(ExceptionUtils.getStackTrace(e));
+        LOG.severe(ExceptionUtils.getRootCauseMessage(e));
+        LOG.severe(ExceptionUtils.getStackTrace(ExceptionUtils.getRootCause(e)));
+    }
+
+    private void setupLogLevel() {
+        LOG.info("Setting logging to log everything.");
+        Enumeration<String> loggerNames = LogManager.getLogManager().getLoggerNames();
+        for(; loggerNames.hasMoreElements();) {
+            String loggerName = loggerNames.nextElement();
+            LogManager.getLogManager().getLogger(loggerName).setLevel(Level.ALL);
+        }
+        LOG.info("Log level has now been set up.");
     }
 
 }
