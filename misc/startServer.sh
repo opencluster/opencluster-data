f390de90acc0ef57da1f9141b7cf618c7c5f1e52
Clinton Webb <webb.clint@gmail.com>
2014-08-02 23:26:32 +0800

    Updated startup script for testing

--- misc/startServer.sh
+++ misc/startServer.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-cd 
-cd dev/opencluster/server/ 
-ocd -l $(ifconfig | grep "inet addr" | head -n 1 | sed 's/:/ /g' | awk '{print $3}'):7777 -g logs/logs- -vvvvvvvv
+
+#cd 
+#cd dev/opencluster/server/ 
+#ocd -l $(ifconfig | grep "inet addr" | head -n 1 | sed 's/:/ /g' | awk '{print $3}'):7777 -g logs/logs- -vvvvvvvv
+
+# get the IP address of the current machine
+IP=$(ip addr | grep "inet"|grep "scope global"|head -n 1 | awk '{print $2}' | awk -F/ '{print $1}')
+
+# create a new conninfo file updated with the IP addr.
+conninfo_set node_a.conninfo.src "ip" "$IP">node_a.conninfo
+
+ocd -l node_a.conninfo -g logs/logs- -vvvvvvvv
+
+
+
